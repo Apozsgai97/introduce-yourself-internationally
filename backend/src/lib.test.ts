021416fe5e4a1a7f0aa5782f58081f162fe51d62
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-12 16:31:21 +0200

    Make an error if the language invalid

--- backend/src/lib.test.ts
+++ backend/src/lib.test.ts
@@ -2,16 +2,27 @@ import test from "node:test";
 import { deepEqual } from "node:assert/strict";
 import { makeIntroduction } from "./lib";
 
-test("if no language return an error", () => {
+test("if swedish language return the text", () => {
   const introductions = {
     language: "swedish",
     text: "Hej! Jag heter NAME. Trevligt att träffa dig! Vad heter du?",
   };
-  
+
   const result = makeIntroduction(introductions.text, "swedish");
 
   deepEqual(
     result,
     "Hej! Jag heter NAME. Trevligt att träffa dig! Vad heter du?"
   );
+});
+
+test("if invalid language return an error", () => {
+  const introductions = {
+    language: "swedish",
+    text: "Hej! Jag heter NAME. Trevligt att träffa dig! Vad heter du?",
+  };
+
+  const result = makeIntroduction(introductions.text, "");
+
+  deepEqual(result, "Language not found. Write something else.");
 });
--- backend/src/lib.ts
+++ backend/src/lib.ts
@@ -4,6 +4,13 @@ export function makeIntroduction(text: string, language: string){
   case "swedish":
    return text;
    break;
+  case "english":
+   return text;
+   break;
+  case "hungarian":
+   return text;
+  case "spanish":
+   return text;
   default:
    return "Language not found. Write something else."
  }
