5fadef21c8c406f5170b9d78f4d44f62c5565a27
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-12 15:31:23 +0200

    write out the language if the form submitted

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,32 @@
 import http from "http";
+import querystring from "querystring";
 
-export function main(){
- http.createServer(function(req, res){
-  res.writeHead(200, {
-   "Content-Type": "application/json",
-   "Access-control-allow-origin": "*",
-   "Access-control-allow-methods":"OPTIONS, POST, GET",
-   "access-control-max-age": "2592000", //Fix this later
-  });
-  const result = JSON.stringify({ok: true});
-  res.write(result);
-  res.end();
- })
- .listen(8080);
-};
+export function main() {
+  http
+    .createServer(function (req, res) {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        try {
+          const parsedData = querystring.parse(body);
+          res.writeHead(200, {
+            "Content-Type": "application/json",
+            "Access-control-allow-origin": "*",
+            "Access-control-allow-methods": "OPTIONS, POST, GET",
+            "access-control-max-age": "2592000",
+          });
+          const result = JSON.stringify(parsedData);
+          res.write(result);
+          res.end();
+        } catch (error) {
+          if (!res.headersSent) {
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            res.end("Invalid JSON");
+          }
+        }
+      });
+    })
+    .listen(8080);
+}
