357019ab2587c56fe33cd58050e7bb0cbaa56bcf
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-10-13 11:59:46 +0200

    change the code to use SQLite

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
 import http from "http";
 import querystring from "querystring";
 import { makeIntroduction, addUsername } from "./lib";
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("database.db")
 
 export interface Introduction {
 language: string,
@@ -37,19 +40,33 @@ export function main() {
         try {
           const parsedData = querystring.parse(body);
           const language = parsedData.language as string;
-          const name = parsedData.name as string
-         
-          res.writeHead(200, {
-            "Content-Type": "application/json",
-            "Access-control-allow-origin": "*",
-            "Access-control-allow-methods": "OPTIONS, POST, GET",
-            "access-control-max-age": "2592000",
-          });
+          const name = parsedData.name as string;
 
-          const introductionText = makeIntroduction(introductions, language);
-          const textWithUserName = addUsername(introductionText, name);
-          res.write(textWithUserName);
-          res.end();
+          db.get("SELECT text FROM introduction WHERE language = ?", [language.toLowerCase()],
+          (err, row: Introduction) => {
+           if(err) {
+            res.writeHead(500, {"Content-Types": "text/plain"});
+            res.end("Database error");
+            return;
+           }
+
+           if (row) {
+            const introductionText = row.text;
+            const textWithUserName = addUsername(introductionText, name);
+
+            res.writeHead(200, {
+              "Content-Type": "application/json",
+              "Access-control-allow-origin": "*",
+              "Access-control-allow-methods": "OPTIONS, POST, GET",
+              "access-control-max-age": "2592000",
+            });
+            res.write(textWithUserName);
+            res.end();
+           }
+          })
+   
+          //const introductionText = makeIntroduction(introductions, language);
+          //const textWithUserName = addUsername(introductionText, name);
         } catch (error) {
           if (!res.headersSent) {
             res.writeHead(400, { "Content-Type": "text/plain" });
